bbaabe6f2cd33e81f8644cbfed773c5d02d152e9
Emilio Ferraro <emilio_1986win@hotmail.com>
2021-11-21 23:41:58 -0300

    initial commit

---
+++ src/main/java/alkemy/Disney2/Disney2/Disney2Application.java
@@ -0,0 +1,13 @@
+package alkemy.Disney2.Disney2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Disney2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Disney2Application.class, args);
+	}
+
+}
---
+++ src/test/java/alkemy/Disney2/Disney2/Disney2ApplicationTests.java
@@ -0,0 +1,13 @@
+package alkemy.Disney2.Disney2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Disney2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
